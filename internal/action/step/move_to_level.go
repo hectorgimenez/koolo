b63ee26f5c6b8885cea090c8ee268006c5dc3ca1
Hector Gimenez <hector.fwbz@gmail.com>
2022-02-19 13:21:08 +0100

    Move to Level

--- internal/action/step/move_to_level.go
+++ internal/action/step/move_to_level.go
@@ -1,28 +1,59 @@
 package step
 
 import (
+	"fmt"
 	"github.com/hectorgimenez/koolo/internal/game"
+	"github.com/hectorgimenez/koolo/internal/helper"
+	"github.com/hectorgimenez/koolo/internal/hid"
+	"time"
 )
 
-type MoveToLevelStep struct {
+type MoveToAreaStep struct {
 	basicStep
-	level                 game.Level
+	area                  game.Area
 	waitingForInteraction bool
 }
 
-func MoveToLevel(level game.Level) *MoveToLevelStep {
-	return &MoveToLevelStep{
+func MoveToLevel(area game.Area) *MoveToAreaStep {
+	return &MoveToAreaStep{
 		basicStep: newBasicStep(),
-		level:     level,
+		area:      area,
 	}
 }
 
-func (m *MoveToLevelStep) Status(data game.Data) Status {
-	//TODO implement me
-	panic("implement me")
+func (m *MoveToAreaStep) Status(data game.Data) Status {
+	// Give some extra time to render the UI
+	if data.Area == m.area && time.Since(m.lastRun) > time.Second*1 {
+		return m.tryTransitionStatus(StatusCompleted)
+	}
+
+	return m.status
 }
 
-func (m *MoveToLevelStep) Run(data game.Data) error {
-	//TODO implement me
-	panic("implement me")
+func (m *MoveToAreaStep) Run(data game.Data) error {
+	m.tryTransitionStatus(StatusInProgress)
+	if time.Since(m.lastRun) < time.Millisecond*500 {
+		return nil
+	}
+
+	if m.waitingForInteraction && time.Since(m.lastRun) < time.Second*3 {
+		return nil
+	}
+
+	m.lastRun = time.Now()
+	for _, l := range data.AdjacentLevels {
+		if l.Area == m.area {
+			path, distance, _ := helper.GetPathToDestination(data, l.Position.X, l.Position.Y-2)
+			if distance > 15 {
+				helper.MoveThroughPath(path, 15, true)
+				return nil
+			}
+			helper.MoveThroughPath(path, 0, false)
+			hid.Click(hid.LeftButton)
+			m.waitingForInteraction = true
+			return nil
+		}
+	}
+
+	return fmt.Errorf("area %s not found", m.area)
 }
