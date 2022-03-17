f336b3d60668c7d6299b27622cf515bc69d08c09
Hector Gimenez <hector.fwbz@gmail.com>
2022-03-17 19:46:37 +0100

    Screenshot captures only game region

--- internal/helper/screenshot.go
+++ internal/helper/screenshot.go
@@ -3,6 +3,7 @@ package helper
 import (
 	"fmt"
 	"github.com/go-vgo/robotgo"
+	"github.com/hectorgimenez/koolo/internal/hid"
 	"os"
 	"time"
 )
@@ -16,5 +17,14 @@ func Screenshot() {
 	}
 
 	fileName := fmt.Sprintf("screenshots/error-%s.png", time.Now().Format("2006-01-02 15_04_05"))
-	robotgo.SaveCapture(fileName)
+
+	scale := robotgo.ScaleF()
+
+	startX := int(float64(hid.WindowLeftX) * scale)
+	startY := int(float64(hid.WindowTopY) * scale)
+
+	width := int(float64(hid.GameAreaSizeX) * scale)
+	height := int(float64(hid.GameAreaSizeY) * scale)
+
+	robotgo.SaveCapture(fileName, startX, startY, width, height)
 }
