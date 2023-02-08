97ce4927020d72771f3b33684c29c5d472fe078d
Hector Gimenez <hector.fwbz@gmail.com>
2023-02-08 22:38:14 +0900

    Added koolo-map to build script

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 go build -tags static --ldflags -extldflags="-static" -o build/koolo.exe ./cmd/koolo/main.go
 xcopy /E /I /y config build\config
 xcopy /y rustdecrypt.dll build
+xcopy /y koolo-map.exe build
