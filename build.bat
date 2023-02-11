49a0245928f46f85b92d4acdaef97e491563f7f3
Hector Gimenez <hector.fwbz@gmail.com>
2023-02-11 13:45:02 +0900

    Add registry file for d2lod installation path

--- build.bat
+++ build.bat
@@ -2,3 +2,4 @@ go build -tags static --ldflags -extldflags="-static" -o build/koolo.exe ./cmd/k
 xcopy /E /I /y config build\config
 xcopy /y rustdecrypt.dll build
 xcopy /y koolo-map.exe build
+xcopy /y d2.install.reg build
