e36aed0f285dc1cbd3ab7ff8d75aa9b73acd23a8
Hector Gimenez <hector.fwbz@gmail.com>
2023-05-08 21:15:39 +0900

    Update build script

--- build.bat
+++ build.bat
@@ -1,5 +1,9 @@
+rmdir /s /q build
 go build -tags static --ldflags -extldflags="-static" -o build/koolo.exe ./cmd/koolo/main.go
-xcopy /E /I /y config build\config
+mkdir build\config
+copy config\config.yaml.dist build\config\config.yaml
+xcopy /E /I /y config\pickit build\config\pickit
 xcopy /y rustdecrypt.dll build
 xcopy /y koolo-map.exe build
 xcopy /y d2.install.reg build
+xcopy /y README.md build
