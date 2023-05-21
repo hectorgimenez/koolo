e346e7078d19c8707f4e3a45a9398202565005a2
Hector Gimenez <hector.fwbz@gmail.com>
2023-05-21 14:42:39 +0900

    Act 2 Leveling

--- internal/run/leveling_act2.go
+++ internal/run/leveling_act2.go
@@ -1,6 +1,10 @@
 package run
 
-import "github.com/hectorgimenez/koolo/internal/action"
+import (
+	"github.com/hectorgimenez/d2go/pkg/data"
+	"github.com/hectorgimenez/d2go/pkg/data/area"
+	"github.com/hectorgimenez/koolo/internal/action"
+)
 
 func (a Leveling) act2() (actions []action.Action) {
 	actions = append(actions,
@@ -12,9 +16,36 @@ func (a Leveling) act2() (actions []action.Action) {
 }
 
 func (a Leveling) radament() action.Action {
-	return nil
+	return action.NewChain(func(d data.Data) (actions []action.Action) {
+		actions = append(actions,
+			a.builder.WayPoint(area.SewersLevel2Act2),
+			a.builder.MoveToAreaAndKill(area.SewersLevel3Act2),
+		)
+
+		// TODO: Find Radament (use 355 object to locate him)
+		return
+	})
 }
 
 func (a Leveling) findHoradricCube() action.Action {
-	return nil
+	return action.NewChain(func(d data.Data) []action.Action {
+		for _, i := range d.Items.Inventory {
+			if i.Name == "HoradricCube" {
+				a.logger.Info("Horadric Cube found, skipping quest")
+				return nil
+			}
+		}
+
+		a.logger.Info("Horadric Cube not found, starting quest")
+		return []action.Action{
+			a.builder.MoveToAreaAndKill(area.RockyWaste),
+			a.builder.MoveToAreaAndKill(area.DryHills),
+			a.builder.DiscoverWaypoint(),
+			a.builder.MoveToAreaAndKill(area.HallsOfTheDeadLevel1),
+			a.builder.MoveToAreaAndKill(area.HallsOfTheDeadLevel2),
+			a.builder.DiscoverWaypoint(),
+			a.builder.MoveToAreaAndKill(area.HallsOfTheDeadLevel3),
+			a.builder.ClearArea(true, data.MonsterAnyFilter()),
+		}
+	})
 }
