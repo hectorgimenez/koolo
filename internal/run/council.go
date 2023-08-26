8cb5cbb8459906b9cd34806676eaa75d3297ca54
Hector Gimenez <hector.fwbz@gmail.com>
2023-08-26 15:41:46 +0900

    use MoveTo action

--- internal/run/council.go
+++ internal/run/council.go
@@ -4,7 +4,6 @@ import (
 	"github.com/hectorgimenez/d2go/pkg/data"
 	"github.com/hectorgimenez/d2go/pkg/data/area"
 	"github.com/hectorgimenez/koolo/internal/action"
-	"github.com/hectorgimenez/koolo/internal/action/step"
 )
 
 type Council struct {
@@ -19,17 +18,16 @@ func (s Council) BuildActions() []action.Action {
 	return []action.Action{
 		s.builder.WayPoint(area.Travincal), // Moving to starting point (Travincal)
 		s.char.Buff(),                      // Buff
-		action.BuildStatic(func(d data.Data) (steps []step.Step) {
+		s.builder.MoveTo(func(d data.Data) (data.Position, bool) {
 			for _, al := range d.AdjacentLevels {
 				if al.Area == area.DuranceOfHateLevel1 {
-					steps = append(steps, step.MoveTo(data.Position{
+					return data.Position{
 						X: al.Position.X - 1,
 						Y: al.Position.Y + 3,
-					}))
+					}, true
 				}
 			}
-
-			return
+			return data.Position{}, false
 		}),
 		s.char.KillCouncil(), // Kill Council
 	}
