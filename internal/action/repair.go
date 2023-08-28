e727a012f4883a4854f666124349822368aa77d2
Hector Gimenez <hector.fwbz@gmail.com>
2023-08-28 22:24:36 +0900

    fix repair

--- internal/action/repair.go
+++ internal/action/repair.go
@@ -12,23 +12,22 @@ import (
 	"github.com/hectorgimenez/koolo/internal/town"
 )
 
-func (b Builder) Repair() *Chain {
-	return NewChain(func(d data.Data) []Action {
+func (b Builder) Repair() *Factory {
+	return NewFactory(func(d data.Data) Action {
 		for _, i := range d.Items.ByLocation(item.LocationEquipped) {
-			if du, found := i.Stats[stat.Durability]; found && du.Value <= 1 {
+			du, found := i.Stats[stat.Durability]
+			if _, maxDurabilityFound := i.Stats[stat.MaxDurability]; maxDurabilityFound && !found || (found && du.Value <= 1) {
 				b.logger.Info(fmt.Sprintf("Repairing %s, durability is: %d", i.Name, du.Value))
-				return []Action{
-					b.InteractNPC(town.GetTownByArea(d.PlayerUnit.Area).RepairNPC(),
-						step.KeySequence("home", "down", "enter"),
-						step.SyncStep(func(_ data.Data) error {
-							helper.Sleep(100)
-							hid.MovePointer(390, 515)
-							hid.Click(hid.LeftButton)
-							helper.Sleep(500)
-							return nil
-						}),
-					),
-				}
+				return b.InteractNPC(town.GetTownByArea(d.PlayerUnit.Area).RepairNPC(),
+					step.KeySequence("home", "down", "enter"),
+					step.SyncStep(func(_ data.Data) error {
+						helper.Sleep(100)
+						hid.MovePointer(390, 515)
+						hid.Click(hid.LeftButton)
+						helper.Sleep(500)
+						return nil
+					}),
+				)
 			}
 		}
 
