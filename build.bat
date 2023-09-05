3448d10209ccd469dbc26358d17258c12cd5ad86
Hector Gimenez <hector.fwbz@gmail.com>
2023-09-05 21:11:24 +0900

    upgrade build script

--- build.bat
+++ build.bat
@@ -5,16 +5,17 @@ echo Cleaning up previous artifacts...
 if exist build rmdir /s /q build > NUL || goto :error
 
 echo Building Koolo binary...
+set PATH=%PATH%;C:\Program Files (x86)\CMake\bin;C:\mingw-w64\x86_64-8.1.0-posix-seh-rt_v6-rev0\mingw64\bin;C:\opencv\build\install\x64\mingw\bin
 go build -trimpath -tags static --ldflags -extldflags="-static" -o build/koolo.exe ./cmd/koolo/main.go > NUL || goto :error
 
 echo Copying assets...
 mkdir build\config > NUL || goto :error
 copy config\config.yaml.dist build\config\config.yaml  > NUL || goto :error
 xcopy /q /E /I /y config\pickit build\config\pickit  > NUL || goto :error
+xcopy /q /E /I /y config\pickit_leveling build\config\pickit_leveling  > NUL || goto :error
 xcopy /q /E /I /y assets build\assets  > NUL || goto :error
 xcopy /q /y rustdecrypt.dll build  > NUL || goto :error
 xcopy /q /y koolo-map.exe build > NUL || goto :error
-xcopy /q /y d2.install.reg build > NUL || goto :error
 xcopy /q /y README.md build > NUL || goto :error
 
 echo Done! Artifacts are in build directory.
