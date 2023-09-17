14c6e058bb9c41fc6cb7e426e55bed952f2e4085
Hector Gimenez <hector.fwbz@gmail.com>
2023-09-17 21:05:35 +0900

    don't buff if wp destination is town (for example act 5 before entering pindleskin portal)

--- internal/action/waypoint.go
+++ internal/action/waypoint.go
@@ -99,9 +99,11 @@ func (b *Builder) useWP(a area.Area) *Chain {
 		b.logger.Info("Traversing areas to reach destination", zap.Any("areas", traverseAreas))
 
 		for i, dst := range traverseAreas {
-			actions = append(actions,
-				b.ch.Buff(),
-			)
+			if !dst.IsTown() {
+				actions = append(actions,
+					b.ch.Buff(),
+				)
+			}
 
 			if i > 0 {
 				actions = append(actions,
