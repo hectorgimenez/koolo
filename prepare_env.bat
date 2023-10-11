ede2434c10f445271c56570e4947fcfb43203b4e
Hector Gimenez <hector.fwbz@gmail.com>
2023-10-11 22:45:08 +0900

    CI testing

--- prepare_env.bat
+++ prepare_env.bat
@@ -1,9 +1,7 @@
+@echo off
 
 echo Installing dependencies...
 go mod download
-
-echo %GOPATH%
-dir %GOPATH%\pkg\mod
 
 echo Building OpenCV... (this will take a while)
 %GOPATH%\pkg\mod\gocv.io\x\gocv@v0.33.0\win_build_opencv.cmd static
