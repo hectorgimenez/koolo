213e32af3d468eac37dcf3110197215d963b0dd5
13413j1j13j5315n13 <gopalito1@protonmail.com>
2024-02-19 13:34:46 +0200

    Refactor follower run to use actions with RepeatUntilNoSteps, implement using entrances, and WP to leader

--- internal/run/follower.go
+++ internal/run/follower.go
@@ -3,16 +3,21 @@ package run
 import (
 	"fmt"
 	"github.com/hectorgimenez/d2go/pkg/data"
+	"github.com/hectorgimenez/d2go/pkg/data/area"
 	"github.com/hectorgimenez/koolo/internal/action"
 	"github.com/hectorgimenez/koolo/internal/action/step"
 	"github.com/hectorgimenez/koolo/internal/config"
 	"github.com/hectorgimenez/koolo/internal/pather"
+	"time"
 )
 
 const NameFollower = "Follower"
 const MaxCoordinateDiff = 100
+const EntranceMaxDiff = 15
+const EntranceMaxSecondsDelay = time.Second * 5
 
 var leaderNotFoundInformed = false
+var lastEntranceEntered = time.Now()
 
 type Follower struct {
 	baseRun
@@ -27,7 +32,7 @@ func (f Follower) BuildActions() []action.Action {
 	// More actions to be added later
 
 	return []action.Action{
-		action.NewStepChain(func(d data.Data) []step.Step {
+		action.NewChain(func(d data.Data) []action.Action {
 			leaderRosterMember, found := d.Roster.FindByName(config.Config.Follower.LeaderName)
 			if !found {
 				if !leaderNotFoundInformed {
@@ -36,16 +41,54 @@ func (f Follower) BuildActions() []action.Action {
 				}
 
 				// When leader has not been found, it is NOT an error situation. Just wait
-				return []step.Step{step.Wait(100)}
+				return []action.Action{
+					f.builder.Wait(300),
+				}
 			}
 
-			// Is leader too far way? If yes, do not do anything
+			// Is leader too far away?
 			if pather.DistanceFromMe(d, leaderRosterMember.Position) > MaxCoordinateDiff {
-				return []step.Step{step.Wait(100)}
+				// If we have an entrance, use it
+				entrance := getClosestEntrances(d)
+				if entrance != nil && time.Since(lastEntranceEntered) > EntranceMaxSecondsDelay {
+					lastEntranceEntered = time.Now()
+
+					return []action.Action{
+						f.builder.MoveToArea(entrance.Area),
+					}
+				}
+
+				// Is leader in the same act and in a waypoint location? Let's use waypoint to that location
+				_, wpAreaFound := area.WPAddresses[leaderRosterMember.Area]
+				if d.PlayerUnit.Area.Act() == leaderRosterMember.Area.Act() && d.PlayerUnit.Area.IsTown() && wpAreaFound {
+					return []action.Action{
+						f.builder.WayPoint(leaderRosterMember.Area),
+						f.builder.Wait(time.Second * 1),
+					}
+				}
+
+				return []action.Action{
+					f.builder.Wait(100),
+				}
 			}
 
-			return []step.Step{step.MoveTo(leaderRosterMember.Position)}
-
+			return []action.Action{
+				action.NewStepChain(func(d data.Data) []step.Step {
+					return []step.Step{step.MoveTo(leaderRosterMember.Position)}
+				}),
+				f.builder.Wait(100),
+			}
 		}, action.RepeatUntilNoSteps()),
 	}
+}
+
+func getClosestEntrances(d data.Data) *data.Level {
+	for _, l := range d.AdjacentLevels {
+		distFromMe := pather.DistanceFromMe(d, l.Position)
+		if distFromMe <= EntranceMaxDiff {
+			return &l
+		}
+	}
+
+	return nil
 }
