ccb246db81cbe2c9199e129e652a94b33e9d378d
Arto Simonyan <artosimonyan@protonmail.com>
2024-04-15 15:47:56 +0300

    Revert kill process on exit

--- internal/supervisor.go
+++ internal/supervisor.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
-	"os"
 	"time"
 
 	"github.com/hectorgimenez/koolo/internal/container"
@@ -67,14 +66,6 @@ func (s *baseSupervisor) Stop() {
 	}
 
 	s.c.Injector.Unload()
-	process, err := os.FindProcess(int(s.c.Reader.Process.GetPID()))
-	if err != nil {
-		s.c.Logger.Info("Failed to find process", slog.String("configuration", s.name))
-	}
-	err = process.Kill()
-	if err != nil {
-		s.c.Logger.Info("Failed to kill process", slog.String("configuration", s.name))
-	}
 	s.c.Logger.Info("Finished stopping", slog.String("configuration", s.name))
 
 }
