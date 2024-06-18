e7f36ae66eed156bec4bd7ca0bfbcf151997e3e3
SoundsLegit <107037010+SoundsLegit@users.noreply.github.com>
2024-06-18 07:13:14 +0200

    Empty cursor (#303)

--- internal/action/item.go
+++ internal/action/item.go
@@ -2,11 +2,12 @@ package action
 
 import (
 	"fmt"
-
 	"github.com/hectorgimenez/d2go/pkg/data"
 	"github.com/hectorgimenez/d2go/pkg/data/item"
 	"github.com/hectorgimenez/d2go/pkg/nip"
+	"github.com/hectorgimenez/koolo/internal/action/step"
 	"github.com/hectorgimenez/koolo/internal/game"
+	"github.com/hectorgimenez/koolo/internal/helper"
 )
 
 func (b *Builder) doesExceedQuantity(i data.Item, rule nip.Rule, d game.Data) bool {
@@ -34,4 +35,24 @@ func (b *Builder) doesExceedQuantity(i data.Item, rule nip.Rule, d game.Data) bo
 	b.Logger.Debug(fmt.Sprintf("For item %s found %d max quantity from pickit rule, number of items in the stash tabs %d", i.Name, maxQuantity, matchedItemsInStash))
 
 	return matchedItemsInStash >= maxQuantity
+}
+
+func (b *Builder) DropMouseItem() *Chain {
+	return NewChain(func(d game.Data) (actions []Action) {
+		result := d.Inventory.ByLocation(item.LocationCursor)
+		if len(result) > 0 {
+			actions = append(actions, NewStepChain(func(d game.Data) []step.Step {
+				return []step.Step{
+					step.SyncStep(func(d game.Data) error {
+						helper.Sleep(1000)
+						b.HID.Click(game.LeftButton, 500, 500)
+						helper.Sleep(1000)
+						return nil
+					}),
+				}
+			}))
+		}
+
+		return actions
+	})
 }
