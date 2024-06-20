594fc41c481251e43df301d1ac67a527053ab9e6
SoundsLegit <107037010+SoundsLegit@users.noreply.github.com>
2024-06-20 11:00:50 +0200

    Added closing the mini panel when switching to legacy

--- internal/action/legacy_mode.go
+++ internal/action/legacy_mode.go
@@ -1,6 +1,7 @@
 package action
 
 import (
+	"github.com/hectorgimenez/koolo/internal/helper"
 	"time"
 
 	"github.com/hectorgimenez/koolo/internal/action/step"
@@ -14,6 +15,13 @@ func (b *Builder) SwitchToLegacyMode() *StepChainAction {
 			return []step.Step{
 				step.KeySequence(b.Reader.GetKeyBindings().LegacyToggle.Key1[0]),
 				step.Wait(time.Millisecond * 500), // Add small delay to allow the game to switch
+				// Close the mini panel
+				step.SyncStep(func(d game.Data) error {
+					helper.Sleep(100)
+					b.HID.Click(game.LeftButton, 639, 686)
+					helper.Sleep(100)
+					return nil
+				}),
 			}
 		}
 		return nil
