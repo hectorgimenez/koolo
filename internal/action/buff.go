5d536d5348ce67defdd3d1966c2b14cc96ff19c0
SoundsLegit <d.melia@outlook.fr>
2024-06-22 21:19:46 +0200

    Removed change from other branch - wind_druid

--- internal/action/buff.go
+++ internal/action/buff.go
@@ -134,9 +134,6 @@ func (b *Builder) IsRebuffRequired(d game.Data) bool {
 			if buff == skill.EnergyShield && !d.PlayerUnit.States.HasState(state.Energyshield) {
 				return true
 			}
-			if buff == skill.CycloneArmor && !d.PlayerUnit.States.HasState(state.Cyclonearmor) {
-				return true
-			}
 		}
 	}
 
