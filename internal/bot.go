6c9df790c40e0b3fd3b95edee40884fb63480e09
Arto Simonyan <artosimonyan@protonmail.com>
2024-07-07 03:24:26 +0300

    Fixed error on settings save or new client launch

--- internal/bot.go
+++ internal/bot.go
@@ -14,6 +14,7 @@ import (
 	"github.com/hectorgimenez/koolo/internal/container"
 	"github.com/hectorgimenez/koolo/internal/event"
 	"github.com/hectorgimenez/koolo/internal/health"
+	"github.com/hectorgimenez/koolo/internal/helper"
 	"github.com/hectorgimenez/koolo/internal/run"
 )
 
@@ -215,13 +216,20 @@ func (b *Bot) Run(ctx context.Context, firstRun bool, runs []run.Run) (err error
 func (b *Bot) maxGameLengthExceeded(startedAt time.Time) error {
 	// Check if config or Characters map is nil
 	if config.Characters == nil {
-		return fmt.Errorf("configuration is not initialized")
+		helper.Sleep(1000) // Wait a second a re-check to avoid configuration saved or reloads
+		if config.Characters == nil {
+			return fmt.Errorf("configuration is not initialized")
+		}
 	}
 
 	// Check if specific supervisor config is nil
 	characterConfig, exists := config.Characters[b.supervisorName]
 	if !exists || characterConfig == nil {
-		return fmt.Errorf("character configuration for %s not found or is nil", b.supervisorName)
+		helper.Sleep(1000) // Wait a second and re-check to avoid configuration saves or reloads resulting in errors
+		characterConfig, exists := config.Characters[b.supervisorName]
+		if !exists || characterConfig == nil {
+			return fmt.Errorf("character configuration for %s not found or is nil", b.supervisorName)
+		}
 	}
 
 	if time.Since(startedAt).Seconds() > float64(config.Characters[b.supervisorName].MaxGameLength) {
