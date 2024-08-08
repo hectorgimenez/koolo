f464a386ae7b216011160009fd1a33be481a66e6
TDL <tdlofcc@gmail.com>
2024-08-08 20:50:54 +0200

    Updated find item function

--- internal/character/berserk_barb.go
+++ internal/character/berserk_barb.go
@@ -2,6 +2,7 @@ package character
 
 import (
 	"log/slog"
+	"sort"
 	"time"
 
 	"github.com/hectorgimenez/d2go/pkg/data"
@@ -111,88 +112,7 @@ func (s Berserker) KillMonsterSequence(
 	}, action.RepeatUntilNoSteps())
 }
 
-// func (s Berserker) KillMonsterSequence(
-// 	monsterSelector func(d game.Data) (data.UnitID, bool),
-// 	skipOnImmunities []stat.Resist,
-// 	opts ...step.AttackOption,
-// ) action.Action {
-// 	var attackAttempts int
-// 	var emptyIterations int
-// 	var findItemAttempts int
-// 	const maxFindItemAttempts = 1
-// 	const maxRange = 15
-// 	const findItemWaitTime = time.Millisecond * 100
-
-// 	return action.NewStepChain(func(d game.Data) (steps []step.Step) {
-// 		id, found := monsterSelector(d)
-// 		if found {
-// 			s.logger.Debug("Monster found", slog.Int("unitID", int(id)))
-// 			emptyIterations = 0
-// 			findItemAttempts = 0
-
-// 			monster, monsterFound := d.Monsters.FindByID(id)
-// 			if !monsterFound {
-// 				s.logger.Debug("Monster data not found")
-// 				return []step.Step{}
-// 			}
-
-// 			if !s.preBattleChecks(d, id, skipOnImmunities) {
-// 				s.logger.Debug("Pre-battle checks failed")
-// 				attackAttempts = 0
-// 				return []step.Step{}
-// 			}
-
-// 			steps = append(steps, step.MoveTo(monster.Position))
-// 			s.logger.Debug("Added step to move towards monster", slog.Any("position", monster.Position))
-
-// 			s.logger.Debug("Adding primary attack step", slog.Int("attemptNumber", attackAttempts+1))
-// 			attackStep := step.PrimaryAttack(id, 1, false, step.Distance(1, maxRange))
-// 			steps = append(steps, step.SyncStep(func(d game.Data) error {
-// 				s.logger.Debug("Executing primary attack", slog.Int("attemptNumber", attackAttempts+1))
-// 				err := attackStep.Run(d, s.container)
-// 				if err != nil {
-// 					s.logger.Debug("Primary attack failed", slog.String("error", err.Error()))
-// 				} else {
-// 					s.logger.Debug("Primary attack executed successfully")
-// 				}
-// 				attackAttempts++
-// 				return err
-// 			}))
-
-// 			if attackAttempts >= 5 {
-// 				s.logger.Debug("Resetting attack sequence after multiple attempts")
-// 				attackAttempts = 0
-// 			}
-// 		} else {
-// 			s.logger.Debug("No targetable monster found")
-// 			attackAttempts = 0
-
-// 			if findItemAttempts < maxFindItemAttempts {
-// 				foundCorpse := s.FindItemOnNearbyCorpses(d, maxRange, findItemWaitTime)
-// 				if foundCorpse {
-// 					findItemAttempts++
-// 					steps = append(steps, step.Wait(findItemWaitTime))
-// 					emptyIterations = 0
-// 				} else {
-// 					emptyIterations++
-// 				}
-// 			} else {
-// 				emptyIterations++
-// 			}
-
-// 			if emptyIterations > 5 {
-// 				s.logger.Debug("No monsters or corpses found for several iterations, exiting sequence")
-// 				return nil
-// 			}
-// 			steps = append(steps, step.Wait(time.Millisecond*100))
-// 		}
-
-// 		s.logger.Debug("Steps created", slog.Int("stepCount", len(steps)))
-// 		return steps
-// 	}, action.RepeatUntilNoSteps())
-// }
-
-func (s Berserker) FindItemOnNearbyCorpses(d game.Data, maxRange int, waitTime time.Duration) bool {
+func (s *Berserker) FindItemOnNearbyCorpses(d game.Data, maxRange int, waitTime time.Duration) bool {
 	s.logger.Debug("Attempting Find Item on nearby corpses", slog.Int("total_corpses", len(d.Corpses)))
 
 	findItemKey, found := d.KeyBindings.KeyBindingForSkill(skill.FindItem)
@@ -205,18 +125,38 @@ func (s Berserker) FindItemOnNearbyCorpses(d game.Data, maxRange int, waitTime t
 	originalPosition := playerPos
 	corpseFound := false
 	successfulFindItems := 0
+	checkedCorpses := make(map[data.UnitID]bool)
+
+	// Sort corpses by distance from the player
+	sort.Slice(d.Corpses, func(i, j int) bool {
+		distI := utils.DistanceFromPoint(playerPos, d.Corpses[i].Position)
+		distJ := utils.DistanceFromPoint(playerPos, d.Corpses[j].Position)
+		return distI < distJ
+	})
 
 	for _, corpse := range d.Corpses {
 		distance := utils.DistanceFromPoint(playerPos, corpse.Position)
 
-		if (corpse.Type != data.MonsterTypeChampion &&
+		if distance > maxRange {
+			s.logger.Debug("Reached corpse beyond maxRange, stopping Find Item process",
+				slog.Int("distance", int(distance)),
+				slog.Int("maxRange", maxRange))
+			break
+		}
+
+		// Check if this corpse has already been processed
+		if checkedCorpses[corpse.UnitID] {
+			s.logger.Debug("Skipping already checked corpse", slog.Any("corpse_id", corpse.UnitID))
+			continue
+		}
+
+		if corpse.Type != data.MonsterTypeChampion &&
 			corpse.Type != data.MonsterTypeMinion &&
 			corpse.Type != data.MonsterTypeUnique &&
-			corpse.Type != data.MonsterTypeSuperUnique) || distance > maxRange {
-			s.logger.Debug("Skipping corpse: not a desired monster type or too far",
+			corpse.Type != data.MonsterTypeSuperUnique {
+			s.logger.Debug("Skipping corpse: not a desired monster type",
 				slog.Any("corpse_id", corpse.UnitID),
-				slog.Any("monster_type", corpse.Type),
-				slog.Int("distance", int(distance)))
+				slog.Any("monster_type", corpse.Type))
 			continue
 		}
 
@@ -231,6 +171,12 @@ func (s Berserker) FindItemOnNearbyCorpses(d game.Data, maxRange int, waitTime t
 			corpse.Position.X, corpse.Position.Y,
 		)
 
+		s.logger.Debug("Find Item screen coordinates",
+			slog.Int("screenX", screenX),
+			slog.Int("screenY", screenY),
+			slog.Any("playerPos", playerPos),
+			slog.Any("corpsePos", corpse.Position))
+
 		s.container.HID.MovePointer(screenX, screenY)
 		time.Sleep(waitTime)
 
@@ -241,6 +187,9 @@ func (s Berserker) FindItemOnNearbyCorpses(d game.Data, maxRange int, waitTime t
 		s.logger.Debug("Find Item used on corpse", slog.Any("corpse_id", corpse.UnitID))
 		time.Sleep(waitTime)
 
+		// Mark this corpse as checked
+		checkedCorpses[corpse.UnitID] = true
+
 		corpseFound = true
 		successfulFindItems++
 
@@ -249,27 +198,13 @@ func (s Berserker) FindItemOnNearbyCorpses(d game.Data, maxRange int, waitTime t
 			break
 		}
 
-		// // Directly check if the cursor is over any unit
-		// if d.HoverData.IsHovered {
-		// 	s.container.HID.Click(game.RightButton, screenX, screenY)
-		// 	s.logger.Debug("Find Item used on corpse", slog.Any("corpse_id", corpse.UnitID))
-		// 	time.Sleep(waitTime)
-
-		// 	corpseFound = true
-		// 	successfulFindItems++
-
-		// 	if d.PlayerUnit.States.HasState(state.Cooldown) {
-		// 		s.logger.Debug("Find Item on cooldown, stopping")
-		// 		break
-		// 	}
-		// } else {
-		// 	s.logger.Debug("Cursor not over any unit, skipping", slog.Any("corpse_id", corpse.UnitID))
-		// }
-
 		playerPos = d.PlayerUnit.Position
 
 		if utils.DistanceFromPoint(originalPosition, playerPos) > maxRange {
-			s.logger.Debug("Moved too far from original position, stopping Find Item")
+			s.logger.Debug("Moved too far from original position, stopping Find Item",
+				slog.Any("originalPosition", originalPosition),
+				slog.Any("currentPosition", playerPos),
+				slog.Int("distance", int(utils.DistanceFromPoint(originalPosition, playerPos))))
 			break
 		}
 	}
