4f3f986f71a0da79dc093057a2fc07d9d0ae3cfe
TDL <TDLofCC@gmail.com>
2024-08-11 10:17:38 +0200

    Added check for town portal keybind

--- internal/character/paladin_leveling.go
+++ internal/character/paladin_leveling.go
@@ -1,6 +1,7 @@
 package character
 
 import (
+	"log/slog"
 	"sort"
 	"time"
 
@@ -19,10 +20,21 @@ type PaladinLeveling struct {
 	BaseCharacter
 }
 
-func (p PaladinLeveling) CheckKeyBindings(d game.Data) []skill.ID {
+func (s PaladinLeveling) CheckKeyBindings(d game.Data) []skill.ID {
+	requireKeybindings := []skill.ID{skill.TomeOfTownPortal}
+	missingKeybindings := []skill.ID{}
 
-	// Not implemented
-	return []skill.ID{}
+	for _, cskill := range requireKeybindings {
+		if _, found := d.KeyBindings.KeyBindingForSkill(cskill); !found {
+			missingKeybindings = append(missingKeybindings, cskill)
+		}
+	}
+
+	if len(missingKeybindings) > 0 {
+		s.logger.Debug("There are missing required key bindings.", slog.Any("Bindings", missingKeybindings))
+	}
+
+	return missingKeybindings
 }
 
 func (p PaladinLeveling) KillMonsterSequence(monsterSelector func(d game.Data) (data.UnitID, bool), skipOnImmunities []stat.Resist, opts ...step.AttackOption) action.Action {
