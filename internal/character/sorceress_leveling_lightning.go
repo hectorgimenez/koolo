5779822eacc810f41799b7c7c2789b00665e7efa
Jan <jan.sankala@playpilot.com>
2024-08-12 17:24:36 +0200

    removed a thing that isnt part of this PR

--- internal/character/sorceress_leveling_lightning.go
+++ internal/character/sorceress_leveling_lightning.go
@@ -57,11 +57,7 @@ func (s SorceressLevelingLightning) SkillsToBind(d game.Data) (skill.ID, []skill
 	if d.PlayerUnit.Skills[skill.GlacialSpike].Level > 0 {
 		mainSkill = skill.GlacialSpike
 	}
-	// For a lightsorc, nova is great
-	if d.PlayerUnit.Skills[skill.Nova].Level > 0 &&
-		mainSkill != skill.GlacialSpike{
-		mainSkill = skill.Nova
-	}
+
 	return mainSkill, skillBindings
 }
 
