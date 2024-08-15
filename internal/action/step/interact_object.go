88bf41cb3a20b1ef1ddf7d0b70e5e1ba512de428
ElectricDosh <84617732+ElectricDosh@users.noreply.github.com>
2024-08-15 23:18:23 +0100

    Update interact_object.go
    - Added timeout functionality to InteractObjectStep in interact_object.go
    - Implemented automatic teleportation to a random point in a 10x10 grid centred on the object after 8 seconds of failed interaction attempts
    - Limited to 5 teleportation attempts
    - Added completion check at the start of each interaction attempt
    - Preserved original object interaction logic

--- internal/action/step/interact_object.go
+++ internal/action/step/interact_object.go
@@ -1,125 +1,174 @@
 package step
 
 import (
-	"fmt"
-	"time"
-
-	"github.com/hectorgimenez/koolo/internal/container"
-	"github.com/hectorgimenez/koolo/internal/game"
-
-	"github.com/hectorgimenez/d2go/pkg/data"
-	"github.com/hectorgimenez/d2go/pkg/data/object"
-	"github.com/hectorgimenez/koolo/internal/helper"
-	"github.com/hectorgimenez/koolo/internal/pather"
+    "fmt"
+    "time"
+    "math/rand"
+    "github.com/hectorgimenez/koolo/internal/container"
+    "github.com/hectorgimenez/koolo/internal/game"
+    "github.com/hectorgimenez/d2go/pkg/data"
+    "github.com/hectorgimenez/d2go/pkg/data/object"
+    "github.com/hectorgimenez/koolo/internal/helper"
+    "github.com/hectorgimenez/koolo/internal/pather"
 )
 
 type InteractObjectStep struct {
-	basicStep
-	objectName            object.Name
-	objectID              data.UnitID
-	waitingForInteraction bool
-	isCompleted           func(game.Data) bool
-	mouseOverAttempts     int
-	currentMouseCoords    data.Position
+    basicStep
+    objectName            object.Name
+    objectID              data.UnitID
+    waitingForInteraction bool
+    isCompleted           func(game.Data) bool
+    mouseOverAttempts     int
+    currentMouseCoords    data.Position
+    startTime             time.Time
+    teleportAttempts      int
 }
 
 func InteractObject(name object.Name, isCompleted func(game.Data) bool) *InteractObjectStep {
-	return &InteractObjectStep{
-		basicStep:   newBasicStep(),
-		objectName:  name,
-		isCompleted: isCompleted,
-	}
+    return &InteractObjectStep{
+        basicStep:   newBasicStep(),
+        objectName:  name,
+        isCompleted: isCompleted,
+        startTime:   time.Now(),
+    }
 }
 
 func InteractObjectByID(ID data.UnitID, isCompleted func(game.Data) bool) *InteractObjectStep {
-	return &InteractObjectStep{
-		basicStep:   newBasicStep(),
-		objectID:    ID,
-		isCompleted: isCompleted,
-	}
+    return &InteractObjectStep{
+        basicStep:   newBasicStep(),
+        objectID:    ID,
+        isCompleted: isCompleted,
+        startTime:   time.Now(),
+    }
 }
 
 func (i *InteractObjectStep) Status(d game.Data, _ container.Container) Status {
-	if i.status == StatusCompleted {
-		return StatusCompleted
-	}
-
-	// If isCompleted is nil, we run it at least once, sometimes there is no good way to check interaction
-	if time.Since(i.lastRun) > time.Second*1 && i.isCompleted == nil && i.waitingForInteraction {
-		i.tryTransitionStatus(StatusCompleted)
-	}
-
-	if i.isCompleted != nil && i.isCompleted(d) {
-		return i.tryTransitionStatus(StatusCompleted)
-	}
-
-	return i.status
+    if i.status == StatusCompleted {
+        return StatusCompleted
+    }
+    if i.isCompleted != nil && i.isCompleted(d) {
+        return i.tryTransitionStatus(StatusCompleted)
+    }
+    return i.status
 }
 
 func (i *InteractObjectStep) Run(d game.Data, container container.Container) error {
-	i.tryTransitionStatus(StatusInProgress)
+    i.tryTransitionStatus(StatusInProgress)
 
-	if i.mouseOverAttempts > maxInteractions {
-		return fmt.Errorf("object %d could not be interacted", i.objectName)
-	}
+    // Check if the interaction is already completed
+    if i.isCompleted != nil && i.isCompleted(d) {
+        i.tryTransitionStatus(StatusCompleted)
+        return nil
+    }
 
-	// Give some time before retrying the interaction
-	if i.waitingForInteraction && time.Since(i.lastRun) < time.Millisecond*500 {
-		return nil
-	}
+    // Check for timeout
+    const timeout = 8 * time.Second
+    const maxTeleportAttempts = 5
 
-	i.lastRun = time.Now()
-	var o data.Object
-	var found bool
+    if time.Since(i.startTime) > timeout {
+        if i.teleportAttempts >= maxTeleportAttempts {
+            return fmt.Errorf("max teleport attempts reached for object %d", i.objectName)
+        }
 
-	if i.objectID != 0 {
-		for _, obj := range d.Objects {
-			if obj.ID == i.objectID {
-				o = obj
-				found = true
-				break
-			}
-		}
-	} else {
-		o, found = d.Objects.FindOne(i.objectName)
-		// Let's try to use our own portal instead of any random portal we find
-		if i.objectName == object.TownPortal {
-			for _, obj := range d.Objects {
-				if obj.Owner == d.PlayerUnit.Name {
-					o = obj
-					found = true
-				}
-			}
-		}
-	}
+        // Timeout exceeded, teleport to a random position near the object
+        var o data.Object
+        var found bool
+        if i.objectID != 0 {
+            for _, obj := range d.Objects {
+                if obj.ID == i.objectID {
+                    o = obj
+                    found = true
+                    break
+                }
+            }
+        } else {
+            o, found = d.Objects.FindOne(i.objectName)
+        }
 
-	if found {
-		objectX := o.Position.X - 2
-		objectY := o.Position.Y - 2
-		if o.IsHovered {
-			container.HID.Click(game.LeftButton, i.currentMouseCoords.X, i.currentMouseCoords.Y)
-			i.waitingForInteraction = true
-			return nil
-		} else {
-			distance := pather.DistanceFromMe(d, o.Position)
-			if distance > 15 {
-				return fmt.Errorf("object is too far away: %d. Current distance: %d", o.Name, distance)
-			}
+        if !found {
+            return fmt.Errorf("object %d not found", i.objectName)
+        }
 
-			mX, mY := container.PathFinder.GameCoordsToScreenCords(d.PlayerUnit.Position.X, d.PlayerUnit.Position.Y, objectX, objectY)
-			// In order to avoid the spiral (super slow and shitty) let's try to point the mouse to the top of the portal directly
-			if i.mouseOverAttempts == 2 && o.Name == object.TownPortal {
-				mX, mY = container.PathFinder.GameCoordsToScreenCords(d.PlayerUnit.Position.X, d.PlayerUnit.Position.Y, objectX-4, objectY-4)
-			}
+        randX := rand.Intn(11) - 5
+        randY := rand.Intn(11) - 5
+        newPos := data.Position{X: o.Position.X + randX, Y: o.Position.Y + randY}
 
-			x, y := helper.Spiral(i.mouseOverAttempts)
-			i.currentMouseCoords = data.Position{X: mX + x, Y: mY + y}
-			container.HID.MovePointer(mX+x, mY+y)
-			i.mouseOverAttempts++
+        moveStep := MoveTo(newPos)
+        err := moveStep.Run(d, container)
+        if err != nil {
+            return fmt.Errorf("failed to teleport: %v", err)
+        }
 
-			return nil
-		}
-	}
+        i.teleportAttempts++
+        i.startTime = time.Now()
+        i.mouseOverAttempts = 0
+        i.waitingForInteraction = false
+        return nil
+    }
 
-	return fmt.Errorf("object %d not found", i.objectName)
+    if i.mouseOverAttempts > maxInteractions {
+        return fmt.Errorf("object %d could not be interacted", i.objectName)
+    }
+
+    if i.waitingForInteraction && time.Since(i.lastRun) < time.Millisecond*500 {
+        return nil
+    }
+
+    i.lastRun = time.Now()
+
+    var o data.Object
+    var found bool
+    if i.objectID != 0 {
+        for _, obj := range d.Objects {
+            if obj.ID == i.objectID {
+                o = obj
+                found = true
+                break
+            }
+        }
+    } else {
+        o, found = d.Objects.FindOne(i.objectName)
+        if i.objectName == object.TownPortal {
+            for _, obj := range d.Objects {
+                if obj.Owner == d.PlayerUnit.Name {
+                    o = obj
+                    found = true
+                }
+            }
+        }
+    }
+
+    if found {
+        objectX := o.Position.X - 2
+        objectY := o.Position.Y - 2
+        if o.IsHovered {
+            container.HID.Click(game.LeftButton, i.currentMouseCoords.X, i.currentMouseCoords.Y)
+            i.waitingForInteraction = true
+            return nil
+        } else {
+            distance := pather.DistanceFromMe(d, o.Position)
+            if distance > 15 {
+                return fmt.Errorf("object is too far away: %d. Current distance: %d", o.Name, distance)
+            }
+            mX, mY := container.PathFinder.GameCoordsToScreenCords(d.PlayerUnit.Position.X, d.PlayerUnit.Position.Y, objectX, objectY)
+            if i.mouseOverAttempts == 2 && o.Name == object.TownPortal {
+                mX, mY = container.PathFinder.GameCoordsToScreenCords(d.PlayerUnit.Position.X, d.PlayerUnit.Position.Y, objectX-4, objectY-4)
+            }
+            x, y := helper.Spiral(i.mouseOverAttempts)
+            i.currentMouseCoords = data.Position{X: mX + x, Y: mY + y}
+            container.HID.MovePointer(mX+x, mY+y)
+            i.mouseOverAttempts++
+            return nil
+        }
+    }
+
+    return fmt.Errorf("object %d not found", i.objectName)
+}
+
+func (i *InteractObjectStep) Reset() {
+    i.basicStep.Reset()
+    i.mouseOverAttempts = 0
+    i.waitingForInteraction = false
+    i.startTime = time.Now()
+    i.teleportAttempts = 0
 }
