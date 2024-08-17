8a595d2e7e5ba69259284e18aa14557167caad31
Arto Simonyan <artosimonyan@protonmail.com>
2024-08-17 19:45:09 +0300

    - Added Drifter Cavern run - Added Duriel run - Added Eldritch run

---
+++ internal/v2/run/drifter_cavern.go
@@ -0,0 +1,48 @@
+package run
+
+import (
+	"github.com/hectorgimenez/d2go/pkg/data"
+	"github.com/hectorgimenez/d2go/pkg/data/area"
+	"github.com/hectorgimenez/koolo/internal/config"
+	"github.com/hectorgimenez/koolo/internal/v2/action"
+	"github.com/hectorgimenez/koolo/internal/v2/context"
+)
+
+type DrifterCavern struct {
+	ctx *context.Status
+}
+
+func NewDriverCavern() *DrifterCavern {
+	return &DrifterCavern{
+		ctx: context.Get(),
+	}
+}
+
+func (s DrifterCavern) Name() string {
+	return string(config.DrifterCavernRun)
+}
+
+func (s DrifterCavern) Run() error {
+
+	// Define a default monster filter
+	monsterFilter := data.MonsterAnyFilter()
+
+	// Update filter if we selected to clear only elites
+	if s.ctx.CharacterCfg.Game.DrifterCavern.FocusOnElitePacks {
+		monsterFilter = data.MonsterEliteFilter()
+	}
+
+	// Use the waypoint
+	err := action.WayPoint(area.GlacialTrail)
+	if err != nil {
+		return err
+	}
+
+	// Move to the correct area
+	if err = action.MoveToArea(area.DrifterCavern); err != nil {
+		return err
+	}
+
+	// Clear the area
+	return action.ClearCurrentLevel(s.ctx.CharacterCfg.Game.DrifterCavern.OpenChests, monsterFilter)
+}
