ec42f82345e7c70c1632f314c54ea05a91487e09
Farmith <farmith.rx@gmail.com>
2024-08-18 13:51:15 +0200

    Added Spider Cavern

---
+++ internal/v2/run/spider_cavern.go
@@ -0,0 +1,48 @@
+package run
+
+import (
+	"github.com/hectorgimenez/d2go/pkg/data"
+	"github.com/hectorgimenez/d2go/pkg/data/area"
+	"github.com/hectorgimenez/koolo/internal/config"
+	"github.com/hectorgimenez/koolo/internal/v2/action"
+	"github.com/hectorgimenez/koolo/internal/v2/context"
+)
+
+type SpiderCavern struct {
+	ctx *context.Status
+}
+
+func NewSpiderCavern() *Tristram {
+	return &Tristram{
+		ctx: context.Get(),
+	}
+}
+
+func (sc SpiderCavern) Name() string {
+	return string(config.TristramRun)
+}
+
+func (sc SpiderCavern) Run() error {
+
+	// Define a default monster filter
+	monsterFilter := data.MonsterAnyFilter()
+
+	// Update filter if we selected to clear only elites
+	if sc.ctx.CharacterCfg.Game.DrifterCavern.FocusOnElitePacks {
+		monsterFilter = data.MonsterEliteFilter()
+	}
+
+	// Use waypoint to Spider Forest
+	err := action.WayPoint(area.SpiderForest)
+	if err != nil {
+		return err
+	}
+
+	// Move to the correct area
+	if err = action.MoveToArea(area.SpiderCavern); err != nil {
+		return err
+	}
+
+	// Clear the area
+	return action.ClearCurrentLevel(sc.ctx.CharacterCfg.Game.DrifterCavern.OpenChests, monsterFilter)
+}
