b837a490cdd48a7fdd023dce5bd848821673adb3
Arto Simonyan <artosimonyan@protonmail.com>
2024-08-28 19:16:48 +0300

    Update flow & data on new game (#460)

--- internal/v2/bot/single_supervisor.go
+++ internal/v2/bot/single_supervisor.go
@@ -89,6 +89,9 @@ func (s *SinglePlayerSupervisor) Start() error {
 			}
 			s.bot.ctx.LastBuffAt = time.Time{}
 			s.logGameStart(runs)
+
+			// Refresh the Game data for a new game
+			s.bot.ctx.GameReader.GetData(true)
 			err = s.bot.Run(ctx, firstRun, runs)
 			if err != nil {
 				if errors.Is(context.Canceled, ctx.Err()) {
@@ -167,7 +170,7 @@ func (s *SinglePlayerSupervisor) HandleOutOfGameFlow() error {
 
 		if s.bot.ctx.CharacterCfg.Game.CreateLobbyGames {
 			retryCount := 0
-			for !s.bot.ctx.Data.IsInLobby {
+			for !s.bot.ctx.GameReader.IsInLobby() {
 
 				// Prevent an infinite loop
 				if retryCount >= 5 && !s.bot.ctx.Data.IsInLobby {
@@ -190,7 +193,7 @@ func (s *SinglePlayerSupervisor) HandleOutOfGameFlow() error {
 			}
 		} else {
 			// TODO: Add logic to check if we're on the online or offline tab and handle it accordingly.
-			if !s.bot.ctx.Data.IsOnline && s.bot.ctx.CharacterCfg.AuthMethod != "None" {
+			if !s.bot.ctx.GameReader.IsOnline() && s.bot.ctx.CharacterCfg.AuthMethod != "None" {
 
 				// Try and click the online tab to re-connect to bnet
 				s.bot.ctx.HID.Click(game.LeftButton, 1090, 32) // click the online button
@@ -199,7 +202,7 @@ func (s *SinglePlayerSupervisor) HandleOutOfGameFlow() error {
 				utils.Sleep(4000)
 
 				// Check again
-				if !s.bot.ctx.Data.IsOnline {
+				if !s.bot.ctx.GameReader.IsOnline() {
 					// We failed to re-connect. Kill the client so it will get re-started automatically.
 					if err := s.KillClient(); err != nil {
 						return err
@@ -222,10 +225,10 @@ func (s *SinglePlayerSupervisor) HandleOutOfGameFlow() error {
 		if s.bot.ctx.CharacterCfg.Game.CreateLobbyGames {
 
 			retryCount := 0
-			for !s.bot.ctx.Data.IsInLobby {
+			for !s.bot.ctx.GameReader.IsInLobby() {
 
 				// Prevent an infinite loop
-				if retryCount >= 5 && !s.bot.ctx.Data.IsInLobby {
+				if retryCount >= 5 && !s.bot.ctx.GameReader.IsInLobby() {
 					return fmt.Errorf("failed to enter bnet lobby after 5 retries")
 				}
 
@@ -249,11 +252,11 @@ func (s *SinglePlayerSupervisor) HandleOutOfGameFlow() error {
 			utils.Sleep(1000)
 
 			for range 5 {
-				if s.bot.ctx.Data.IsInCharSelectionScreen && s.bot.ctx.Data.IsOnline {
+				if s.bot.ctx.Data.IsInCharSelectionScreen && s.bot.ctx.GameReader.IsOnline() {
 					break
 				}
 
-				if s.bot.ctx.Data.IsInLobby {
+				if s.bot.ctx.GameReader.IsInLobby() {
 					// Mission failed
 					s.bot.ctx.HID.PressKey(0x1B) // ESC - to avoid importing win here as well
 					utils.Sleep(1000)
