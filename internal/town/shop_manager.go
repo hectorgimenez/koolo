415eaf181cab76b072bb82c34ed4d755330c5f7b
Arto Simonyan <artosimonyan@protonmail.com>
2024-09-13 19:53:14 +0300

    Adjust operator

--- internal/town/shop_manager.go
+++ internal/town/shop_manager.go
@@ -95,7 +95,7 @@ func ShouldBuyTPs() bool {
 
 	qty, found := portalTome.FindStat(stat.Quantity, 0)
 
-	return qty.Value <= 20 || !found
+	return qty.Value < 20 || !found
 }
 
 func ShouldBuyIDs() bool {
@@ -106,7 +106,7 @@ func ShouldBuyIDs() bool {
 
 	qty, found := idTome.FindStat(stat.Quantity, 0)
 
-	return qty.Value <= 20 || !found
+	return qty.Value < 20 || !found
 }
 
 func ShouldBuyKeys() (int, bool) {
