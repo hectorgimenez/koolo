73c6e686503ea6dabbc8518b270e5e169f784112
Arto Simonyan <artosimonyan@protonmail.com>
2024-09-14 11:39:21 +0300

    Character Profile Naming refactor

--- internal/character/sorceress_leveling.go
+++ internal/character/sorceress_leveling.go
@@ -57,7 +57,7 @@ func (s SorceressLeveling) KillMonsterSequence(
 			return nil
 		}
 
-		if completedAttackLoops >= lightningSorceressMaxAttacksLoop {
+		if completedAttackLoops >= NovaSorceressMaxAttacksLoop {
 			return nil
 		}
 
