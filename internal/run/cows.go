d4552265b062bee2552de2cb7c87a16434e801b1
elobo91 <bolduc_elodie@hotmail.com>
2024-10-14 10:35:01 -0400

    Revert "more fixes for idle"
    This reverts commit 69f9de6954da02e06a4f0687be9e3843d3ad433e.

--- internal/run/cows.go
+++ internal/run/cows.go
@@ -2,6 +2,8 @@ package run
 
 import (
 	"errors"
+	"fmt"
+	"github.com/hectorgimenez/koolo/internal/utils"
 	"slices"
 	"strings"
 
@@ -89,16 +91,28 @@ func (a Cows) getWirtsLeg() error {
 	if err != nil {
 		return err
 	}
-
 	wirtCorpse, found := a.ctx.Data.Objects.FindOne(object.WirtCorpse)
 	if !found {
 		return errors.New("wirt corpse not found")
 	}
-	err = action.InteractObject(wirtCorpse, func() bool {
-		_, found := a.ctx.Data.Inventory.Find("WirtsLeg")
+	maxAttempts := 3
+	for attempt := 1; attempt <= maxAttempts; attempt++ {
+		err = action.InteractObject(wirtCorpse, func() bool {
+			_, found := a.ctx.Data.Inventory.Find("WirtsLeg")
+			return found
+		})
 
-		return found
-	})
+		if err == nil {
+			break
+		}
+
+		if attempt < maxAttempts {
+			a.ctx.Logger.Warn("Failed to interact with Wirt's corpse, retrying...", "attempt", attempt)
+			utils.Sleep(150)
+		} else {
+			return fmt.Errorf("failed to interact with Wirt's corpse after %d attempts: %w", maxAttempts, err)
+		}
+	}
 
 	return action.ReturnTown()
 }
