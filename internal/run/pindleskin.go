519c1abe91c2da998db4d224748edf5e032b432e
elb <51070858+elobo91@users.noreply.github.com>
2024-12-13 06:48:03 -0500

    Pindle will keep distance now

--- internal/run/pindleskin.go
+++ internal/run/pindleskin.go
@@ -1,61 +1,66 @@
 package run
 
 import (
-	"errors"
+    "errors"
 
-	"github.com/hectorgimenez/d2go/pkg/data"
-	"github.com/hectorgimenez/d2go/pkg/data/area"
-	"github.com/hectorgimenez/d2go/pkg/data/object"
-	"github.com/hectorgimenez/koolo/internal/action"
-	"github.com/hectorgimenez/koolo/internal/config"
-	"github.com/hectorgimenez/koolo/internal/context"
+    "github.com/hectorgimenez/d2go/pkg/data"
+    "github.com/hectorgimenez/d2go/pkg/data/area"
+    "github.com/hectorgimenez/d2go/pkg/data/npc"
+    "github.com/hectorgimenez/d2go/pkg/data/object"
+    "github.com/hectorgimenez/koolo/internal/action"
+    "github.com/hectorgimenez/koolo/internal/config"
+    "github.com/hectorgimenez/koolo/internal/context"
 )
 
 var fixedPlaceNearRedPortal = data.Position{
-	X: 5130,
-	Y: 5120,
-}
-
-var pindleSafePosition = data.Position{
-	X: 10058,
-	Y: 13236,
+    X: 5130,
+    Y: 5120,
 }
 
 type Pindleskin struct {
-	ctx *context.Status
+    ctx *context.Status
 }
 
 func NewPindleskin() *Pindleskin {
-	return &Pindleskin{
-		ctx: context.Get(),
-	}
+    return &Pindleskin{
+        ctx: context.Get(),
+    }
 }
 
 func (p Pindleskin) Name() string {
-	return string(config.PindleskinRun)
+    return string(config.PindleskinRun)
 }
 
 func (p Pindleskin) Run() error {
-	err := action.WayPoint(area.Harrogath)
-	if err != nil {
-		return err
-	}
+    // Get to Harrogath
+    err := action.WayPoint(area.Harrogath)
+    if err != nil {
+        return err
+    }
 
-	_ = action.MoveToCoords(fixedPlaceNearRedPortal)
+    // Move near the red portal
+    _ = action.MoveToCoords(fixedPlaceNearRedPortal)
 
-	redPortal, found := p.ctx.Data.Objects.FindOne(object.PermanentTownPortal)
-	if !found {
-		return errors.New("red portal not found")
-	}
+    // Find and use the red portal
+    redPortal, found := p.ctx.Data.Objects.FindOne(object.PermanentTownPortal)
+    if !found {
+        return errors.New("red portal not found")
+    }
 
-	err = action.InteractObject(redPortal, func() bool {
-		return p.ctx.Data.AreaData.Area == area.NihlathaksTemple && p.ctx.Data.AreaData.IsInside(p.ctx.Data.PlayerUnit.Position)
-	})
-	if err != nil {
-		return err
-	}
+    err = action.InteractObject(redPortal, func() bool {
+        return p.ctx.Data.AreaData.Area == area.NihlathaksTemple && p.ctx.Data.AreaData.IsInside(p.ctx.Data.PlayerUnit.Position)
+    })
+    if err != nil {
+        return err
+    }
 
-	_ = action.MoveToCoords(pindleSafePosition)
+    // Get NPCs from cached map data
+    for _, npcData := range p.ctx.Data.Areas[area.NihlathaksTemple].NPCs {
+        if npcData.ID == npc.DefiledWarrior {
+            // Let the character implementation handle the actual attack
+            return p.ctx.Char.KillPindle()
+        }
+    }
 
-	return p.ctx.Char.KillPindle()
+    return errors.New("pindleskin not found")
 }
