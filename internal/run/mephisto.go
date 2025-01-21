241a3f5c953f37035a22d6a3649949950a298b0d
braccali1 <92634889+braccali1@users.noreply.github.com>
2025-01-21 15:11:11 -0500

    Fixed Chest Issue.

--- internal/run/mephisto.go
+++ internal/run/mephisto.go
@@ -2,15 +2,15 @@ package run
 
 import (
 	"fmt"
-	"sort"
+	"slices"
 
 	"github.com/hectorgimenez/d2go/pkg/data"
 	"github.com/hectorgimenez/d2go/pkg/data/area"
 	"github.com/hectorgimenez/d2go/pkg/data/npc"
+	"github.com/hectorgimenez/d2go/pkg/data/object"
 	"github.com/hectorgimenez/koolo/internal/action"
 	"github.com/hectorgimenez/koolo/internal/config"
 	"github.com/hectorgimenez/koolo/internal/context"
-	"github.com/hectorgimenez/koolo/internal/pather"
 	"github.com/hectorgimenez/koolo/internal/utils"
 )
 
@@ -73,40 +73,37 @@ func (m Mephisto) Run() error {
 		return action.ClearCurrentLevel(m.ctx.CharacterCfg.Game.Mephisto.OpenChests, m.CouncilMemberFilter())
 
 	} else if m.ctx.CharacterCfg.Game.Mephisto.OpenChests {
-		rooms := m.ctx.PathFinder.OptimizeRoomsTraverseOrder()
-		for _, r := range rooms {
-			// Find the interactable SuperChest objects
-			var objects []data.Object
-			for _, o := range m.ctx.Data.Objects {
-				if o.IsSuperChest() && o.Selectable && r.IsInside(o.Position) {
-					objects = append(objects, o)
-				}
+
+		// determine chests wanted, need to add stand/rack option to config
+		interactableChests := []object.Name{181, 183, 104, 105, 106, 107}
+
+		// find chests and racks
+		var crs []data.Object
+		for _, o := range m.ctx.Data.Objects {
+			if slices.Contains(interactableChests, o.Name) {
+				m.ctx.Logger.Debug("Found chest/rack at:", "position", o.Position)
+				crs = append(crs, o)
+			}
+		}
+		m.ctx.Logger.Debug("Total chests/racks found", "count", len(crs))
+
+		// Handle each of the chests/racks one by one
+		for _, crIndividual := range crs {
+			// Move to the chest/rack
+			err = action.MoveToCoords(crIndividual.Position)
+			if err != nil {
+				return err
 			}
 
-			// Interact with objects in the order of shortest travel
-			for len(objects) > 0 {
-
-				playerPos := m.ctx.Data.PlayerUnit.Position
-
-				sort.Slice(objects, func(i, j int) bool {
-					return pather.DistanceFromPoint(objects[i].Position, playerPos) <
-						pather.DistanceFromPoint(objects[j].Position, playerPos)
-				})
-
-				// Interact with the closest object
-				closestObject := objects[0]
-				err = action.InteractObject(closestObject, func() bool {
-					object, _ := m.ctx.Data.Objects.FindByID(closestObject.ID)
-					return !object.Selectable
-				})
-				if err != nil {
-					m.ctx.Logger.Warn(fmt.Sprintf("[%s] failed interacting with object [%v] in Area: [%s]", m.ctx.Name, closestObject.Name, m.ctx.Data.PlayerUnit.Area.Area().Name), err)
-				}
-				utils.Sleep(500) // Add small delay to allow the game to open the object and drop the content
-
-				// Remove the interacted container from the list
-				objects = objects[1:]
+			// Interact with the object
+			err = action.InteractObject(crIndividual, func() bool {
+				object, _ := m.ctx.Data.Objects.FindByID(crIndividual.ID)
+				return !object.Selectable
+			})
+			if err != nil {
+				m.ctx.Logger.Warn(fmt.Sprintf("[%s] failed interacting with object [%v] in Area: [%s]", m.ctx.Name, crIndividual.Name, m.ctx.Data.PlayerUnit.Area.Area().Name), err)
 			}
+			utils.Sleep(500) // Add small delay to allow the game to open the object and drop the content
 		}
 	}
 
