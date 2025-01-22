3be4af928d7d97d251237b9c20d1f70d5a33b4f3
Dave <davidprog2011@gmail.com>
2025-01-22 16:17:52 +0100

    Whitespace cleanup

--- internal/run/blood_moor.go
+++ internal/run/blood_moor.go
@@ -4,75 +4,75 @@ import (
 	"github.com/hectorgimenez/d2go/pkg/data"
 	"github.com/hectorgimenez/d2go/pkg/data/area"
 	"github.com/hectorgimenez/koolo/internal/action"
-	"github.com/hectorgimenez/koolo/internal/context"
 	"github.com/hectorgimenez/koolo/internal/config"
+	"github.com/hectorgimenez/koolo/internal/context"
 )
 
 type BloodMoor struct {
-   ctx *context.Status
+	ctx *context.Status
 }
 
 func NewBloodMoor() *BloodMoor {
-   return &BloodMoor{
-      ctx: context.Get(),
-   }
+	return &BloodMoor{
+		ctx: context.Get(),
+	}
 }
 
 func (b BloodMoor) Name() string {
-   return string(config.BloodMoorRun)
+	return string(config.BloodMoorRun)
 }
 
 func (b BloodMoor) Run() error {
-    // Get the monster filter based on configuration
-    monsterFilter := GetMonsterFilter(b)
-    
-    // Shall we open chests?
-    openChests := b.ctx.CharacterCfg.Game.BloodMoor.OpenChests
+	// Get the monster filter based on configuration
+	monsterFilter := GetMonsterFilter(b)
 
-    // Less issues finding the waypoint than the town's exit
-    if err := action.WayPoint(area.ColdPlains); err != nil {
-    	return err
-    }
-	
-   // Buff before we start
-    action.Buff()
-	
-   // Moving to the Blood Moor
-    if err := action.MoveToArea(area.BloodMoor); err != nil {
-    	return err
-    }
-    
-    // If we don't want to just do the Den of Evil, we clear the area
-    if !b.ctx.CharacterCfg.Game.BloodMoor.OnlyClearDenOfEvil {
-        if err := action.ClearCurrentLevel(openChests, monsterFilter); err != nil {
-            return err
-        }
-    }
-    
-    // if we don't want to clear the den of evil, the run is over
-    if !b.ctx.CharacterCfg.Game.BloodMoor.ClearDenOfEvil {
-        return nil
-    }
-    
-    // else, we go to the den of evil and clear it
-    if err := action.MoveToArea(area.DenOfEvil); err != nil {
-    	return err
-    }
-    
-    // Buff before we start
-    action.Buff()
-    
-    if err := action.ClearCurrentLevel(openChests, monsterFilter); err != nil {
-        return err
-    }
+	// Shall we open chests?
+	openChests := b.ctx.CharacterCfg.Game.BloodMoor.OpenChests
 
-    return nil
+	// Less issues finding the waypoint than the town's exit
+	if err := action.WayPoint(area.ColdPlains); err != nil {
+		return err
+	}
+
+	// Buff before we start
+	action.Buff()
+
+	// Moving to the Blood Moor
+	if err := action.MoveToArea(area.BloodMoor); err != nil {
+		return err
+	}
+
+	// If we don't want to just do the Den of Evil, we clear the area
+	if !b.ctx.CharacterCfg.Game.BloodMoor.OnlyClearDenOfEvil {
+		if err := action.ClearCurrentLevel(openChests, monsterFilter); err != nil {
+			return err
+		}
+	}
+
+	// if we don't want to clear the den of evil, the run is over
+	if !b.ctx.CharacterCfg.Game.BloodMoor.ClearDenOfEvil {
+		return nil
+	}
+
+	// else, we go to the den of evil and clear it
+	if err := action.MoveToArea(area.DenOfEvil); err != nil {
+		return err
+	}
+
+	// Buff before we start
+	action.Buff()
+
+	if err := action.ClearCurrentLevel(openChests, monsterFilter); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func GetMonsterFilter(b BloodMoor) data.MonsterFilter {
-    if b.ctx.CharacterCfg.Game.BloodMoor.FocusOnElitePacks {
-        return data.MonsterEliteFilter()
-    } else {
-        return data.MonsterAnyFilter()
-    }
+	if b.ctx.CharacterCfg.Game.BloodMoor.FocusOnElitePacks {
+		return data.MonsterEliteFilter()
+	} else {
+		return data.MonsterAnyFilter()
+	}
 }
