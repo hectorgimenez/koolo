620cf7e7bf1c0ba07d1df8688e6cd27473995a85
braccali1 <92634889+braccali1@users.noreply.github.com>
2025-01-25 18:15:08 -0500

    Change Chest Only Config
    Modified mephisto run. When only open chests is selected, changes will find junglechests, weapon racks, and armor stands.

--- internal/run/mephisto.go
+++ internal/run/mephisto.go
@@ -1,6 +1,10 @@
 package run
 
 import (
+	"fmt"
+	"slices"
+	"sort"
+
 	"github.com/hectorgimenez/d2go/pkg/data"
 	"github.com/hectorgimenez/d2go/pkg/data/area"
 	"github.com/hectorgimenez/d2go/pkg/data/npc"
@@ -8,6 +12,7 @@ import (
 	"github.com/hectorgimenez/koolo/internal/action"
 	"github.com/hectorgimenez/koolo/internal/config"
 	"github.com/hectorgimenez/koolo/internal/context"
+	"github.com/hectorgimenez/koolo/internal/pather"
 	"github.com/hectorgimenez/koolo/internal/utils"
 )
 
@@ -65,11 +70,55 @@ func (m Mephisto) Run() error {
 		return err
 	}
 
-	if m.ctx.CharacterCfg.Game.Mephisto.OpenChests || m.ctx.CharacterCfg.Game.Mephisto.KillCouncilMembers {
+	if m.ctx.CharacterCfg.Game.Mephisto.OpenChests && m.ctx.CharacterCfg.Game.Mephisto.KillCouncilMembers {
 		// Clear the area with the selected options
 		return action.ClearCurrentLevel(m.ctx.CharacterCfg.Game.Mephisto.OpenChests, m.CouncilMemberFilter())
-	}
+	} else if m.ctx.CharacterCfg.Game.Mephisto.OpenChests {
 
+		// determine chests wanted
+		interactableChests := []object.Name{181, 183, 104, 105, 106, 107}
+
+		// find chests and racks
+		var crs []data.Object
+		for _, o := range m.ctx.Data.Objects {
+			if slices.Contains(interactableChests, o.Name) {
+				m.ctx.Logger.Debug("Found chest at:", "position", o.Position)
+				crs = append(crs, o)
+			}
+		}
+		m.ctx.Logger.Debug("Total chests/racks found", "count", len(crs))
+
+		// Interact with objects in the order of shortest travel
+		for len(crs) > 0 {
+
+			playerPos := m.ctx.Data.PlayerUnit.Position
+
+			sort.Slice(crs, func(i, j int) bool {
+				return pather.DistanceFromPoint(crs[i].Position, playerPos) <
+					pather.DistanceFromPoint(crs[j].Position, playerPos)
+			})
+
+			// Interact with the closest object
+			closestObject := crs[0]
+			// Move to the chest/rack
+			err = action.MoveToCoords(closestObject.Position)
+			if err != nil {
+				return err
+			}
+			err = action.InteractObject(closestObject, func() bool {
+				object, _ := m.ctx.Data.Objects.FindByID(closestObject.ID)
+				return !object.Selectable
+			})
+			if err != nil {
+				m.ctx.Logger.Warn(fmt.Sprintf("[%s] failed interacting with object [%v] in Area: [%s]", m.ctx.Name, closestObject.Name, m.ctx.Data.PlayerUnit.Area.Area().Name), err)
+			}
+			utils.Sleep(500) // Add small delay to allow the game to open the object and drop the content
+
+			// Remove the interacted container from the list
+			crs = crs[1:]
+		}
+
+	}
 	if m.ctx.CharacterCfg.Game.Mephisto.ExitToA4 {
 		m.ctx.Logger.Debug("Moving to bridge")
 		action.MoveToCoords(data.Position{X: 17588, Y: 8068})
