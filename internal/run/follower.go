b50b7a76db1e89b49e834fe5271c891db17cdd06
Dave <davidprog2011@gmail.com>
2025-02-16 01:38:37 +0100

    feature: use correct leader portal, act 2 town positioning, and cleanup

--- internal/run/follower.go
+++ internal/run/follower.go
@@ -1,13 +1,11 @@
 package run
 
 import (
+	"errors"
 	"github.com/hectorgimenez/d2go/pkg/data"
 	"github.com/hectorgimenez/d2go/pkg/data/area"
-	"github.com/hectorgimenez/d2go/pkg/data/skill"
-	"github.com/hectorgimenez/d2go/pkg/data/stat"
-	"github.com/hectorgimenez/d2go/pkg/memory"
+	"github.com/hectorgimenez/d2go/pkg/data/npc"
 	"github.com/hectorgimenez/koolo/internal/action"
-	"github.com/hectorgimenez/koolo/internal/action/step"
 	"github.com/hectorgimenez/koolo/internal/config"
 	"github.com/hectorgimenez/koolo/internal/context"
 	"github.com/hectorgimenez/koolo/internal/utils"
@@ -25,7 +23,7 @@ func NewFollower() *Follower {
 	}
 }
 
-const MaxDistanceFromLeader = 25
+const MaxDistanceFromLeader = 30
 
 func (f *Follower) Name() string {
 	return string(config.FollowerRun)
@@ -39,66 +37,14 @@ func (f *Follower) Run() error {
 	f.ctx.Logger.Info("Leader is ", slog.Any("leader", leader))
 
 	for leaderFound {
-		// TODO: Detect hostile player and make it a priority. Safety first.
-		//hostileFound := false
-		//if hostileFound {
-		//	f.KillPlayer("")
-		//}
-		
 		if leader.Area != f.ctx.Data.AreaData.Area {
-			f.ctx.Logger.Info("Leader is not in the same area. Attempting to get to him.")
-			lvl := data.Level{}
-			for _, a := range f.ctx.Data.AdjacentLevels {
-				if a.Area == leader.Area { // Only pick the first entrance
-					lvl = a
-					break // Break immediately after finding first valid entrance
-				}
-			}
-
-			if leader.Area.IsTown() {
-				f.ctx.Logger.Info("Leader is in town. Let's go wait there.")
-				_ = action.ReturnTown()
-				f.goToCorrectTown(leader)
-			} else if lvl.Position.X == 0 && lvl.Position.Y == 0 {
-				f.ctx.Logger.Info("Leader is not in a connecting area, returning to the correct town to use his portal.")
-				_ = action.ReturnTown()
-				f.goToCorrectTown(leader)
-				err := action.UsePortalFrom(leader.Name)
-				if err != nil {
-					utils.Sleep(5000)
-				}
-
-			} else {
-				f.ctx.Logger.Info("Leader is in a connection area. Moving to area ID", slog.Any("area", leader.Area))
-				_ = action.MoveToArea(leader.Area)
-			}
+			f.handleLeaderNotInSameArea(leader)
 		} else if leader.Area == f.ctx.Data.AreaData.Area && !f.ctx.Data.PlayerUnit.Area.IsTown() {
-			_ = action.ClearAreaAroundPosition(leader.Position, MaxDistanceFromLeader, f.ctx.Data.MonsterFilterAnyReachable())
-			_ = action.MoveToCoords(leader.Position)
-			action.Buff()
-
-			for _, o := range f.ctx.Data.Objects {
-				if o.IsChest() && o.Selectable && f.isChestInRange(o) {
-					err := action.MoveToCoords(o.Position)
-					if err != nil {
-						f.ctx.Logger.Warn("Failed moving to chest: %v", err)
-						continue
-					}
-					err = action.InteractObject(o, func() bool {
-						chest, _ := f.ctx.Data.Objects.FindByID(o.ID)
-						return !chest.Selectable
-					})
-					if err != nil {
-						f.ctx.Logger.Warn("Failed interacting with chest: %v", err)
-					}
-					utils.Sleep(500) // Add small delay to allow the game to open the chest and drop the content
-				}
-			}
-
+			f.handleLeaderInSameArea(leader)
 		} else if leader.Area == f.ctx.Data.AreaData.Area && f.ctx.Data.PlayerUnit.Area.IsTown() {
-			f.ctx.Logger.Info("We followed the Leader to town, let's wait.")
-			utils.Sleep(5000)
+			f.handleLeaderInTown()
 		}
+
 		f.ctx.RefreshGameData()
 		utils.Sleep(200)
 		leader, leaderFound = f.ctx.Data.Roster.FindByName(f.ctx.CharacterCfg.Companion.LeaderName)
@@ -110,12 +56,101 @@ func (f *Follower) Run() error {
 	return nil
 }
 
+func (f *Follower) handleLeaderNotInSameArea(leader data.RosterMember) {
+	if leader.Area.IsTown() {
+		f.ctx.Logger.Info("Leader is in town. Let's go wait there.")
+		_ = action.ReturnTown()
+		f.goToCorrectTown(leader)
+		utils.Sleep(200)
+		return
+	}
+
+	f.ctx.Logger.Info("Leader is not in the same area. Attempting to get to him.")
+	entrances := f.getEntrancesToLeader(leader)
+	lvl := f.findClosestEntrance(entrances)
+
+	if lvl == nil || (lvl.Position.X == 0 && lvl.Position.Y == 0) {
+		f.handleNoValidEntrance(leader)
+	} else {
+		f.ctx.Logger.Info("Leader is in a connection area. Moving to area ID", slog.Any("area", lvl.Area))
+		_ = action.MoveToCoords(lvl.Position)
+	}
+}
+
+func (f *Follower) getEntrancesToLeader(leader data.RosterMember) []data.Level {
+	var entrances []data.Level
+	for _, al := range f.ctx.Data.AdjacentLevels {
+		if al.Area == leader.Area { // Only pick the first entrance
+			entrances = append(entrances, al)
+		}
+	}
+	return entrances
+}
+
+func (f *Follower) findClosestEntrance(entrances []data.Level) *data.Level {
+	var lvl *data.Level
+	minDistance := math.MaxFloat64 // Start with the largest possible distance
+
+	for _, e := range entrances {
+		dist := calculateDistance(f.ctx.Data.PlayerUnit.Position, e.Position)
+		if dist < minDistance {
+			minDistance = dist
+			lvl = &e
+		}
+	}
+	return lvl
+}
+
+func (f *Follower) handleNoValidEntrance(leader data.RosterMember) {
+	f.ctx.Logger.Info("Leader is not in a connecting area, returning to the correct town to use his portal.")
+	_ = action.ReturnTown()
+	f.goToCorrectTown(leader)
+
+	err := f.UseCorrectPortalFromLeader(leader)
+	if err != nil {
+		utils.Sleep(5000)
+	}
+}
+
+func (f *Follower) handleLeaderInSameArea(leader data.RosterMember) {
+	_ = action.ClearAreaAroundPosition(leader.Position, MaxDistanceFromLeader, f.ctx.Data.MonsterFilterAnyReachable())
+	_ = action.MoveToCoords(leader.Position)
+	action.Buff()
+	f.interactWithChests()
+}
+
+func (f *Follower) interactWithChests() {
+	for _, o := range f.ctx.Data.Objects {
+		if o.IsChest() && o.Selectable && f.isChestInRange(o) {
+			if err := f.moveToChestAndInteract(o); err != nil {
+				f.ctx.Logger.Warn("Failed interacting with chest: %v", err)
+			}
+		}
+	}
+}
+
+func (f *Follower) moveToChestAndInteract(o data.Object) error {
+	if err := action.MoveToCoords(o.Position); err != nil {
+		return err
+	}
+	return action.InteractObject(o, func() bool {
+		chest, _ := f.ctx.Data.Objects.FindByID(o.ID)
+		return !chest.Selectable
+	})
+}
+
+func (f *Follower) handleLeaderInTown() {
+	f.ctx.Logger.Info("We followed the Leader to town, let's wait.")
+	utils.Sleep(5000)
+}
+
 func (f *Follower) goToCorrectTown(leader data.RosterMember) {
 	switch act := leader.Area.Act(); act {
 	case 1:
 		_ = action.WayPoint(area.RogueEncampment)
 	case 2:
 		_ = action.WayPoint(area.LutGholein)
+		_ = action.MoveTo(f.getAtmaPosition)
 	case 3:
 		_ = action.WayPoint(area.KurastDocks)
 	case 4:
@@ -127,65 +162,41 @@ func (f *Follower) goToCorrectTown(leader data.RosterMember) {
 	}
 }
 
+func (f *Follower) getAtmaPosition() (data.Position, bool) {
+	atma, found := f.ctx.Data.NPCs.FindOne(npc.Atma)
+	if found {
+		return atma.Positions[0], true
+	}
+	return data.Position{}, false
+}
+
+func calculateDistance(pos1, pos2 data.Position) float64 {
+	dx := pos1.X - pos2.X
+	dy := pos1.Y - pos2.Y
+	return math.Sqrt(float64(dx*dx + dy*dy))
+}
+
 func (f *Follower) isChestInRange(chest data.Object) bool {
-	// Calculate the distance between the chest and the player
 	distanceX := chest.Position.X - f.ctx.Data.PlayerUnit.Position.X
 	distanceY := chest.Position.Y - f.ctx.Data.PlayerUnit.Position.Y
 	distance := math.Sqrt(float64(distanceX*distanceX + distanceY*distanceY))
-
-	// Check if the distance is within the allowed range
 	return distance <= MaxDistanceFromLeader
 }
 
-var isHostile = false
+func (f *Follower) UseCorrectPortalFromLeader(leader data.RosterMember) error {
+	ctx := context.Get()
+	ctx.SetLastAction("UsePortalFrom")
 
-// TODO: Make flag available in D2GO, then implement the following functions
-func (f *Follower) isHostile(value bool) {
-	isHostile = value
-}
+	if !ctx.Data.PlayerUnit.Area.IsTown() {
+		return nil
+	}
 
-func (f *Follower) KillPlayer(playerName string) bool {
-	step.SwapToCTA()
-	action.Buff()
-	step.SwapToMainWeapon()
+	for _, obj := range ctx.Data.Objects {
+		if obj.IsPortal() && obj.Owner == leader.Name && obj.PortalData.DestArea == leader.Area {
 
-	for isHostile {
-		var target = memory.RawPlayerUnit{}
-		for _, p := range f.ctx.GameReader.GetRawPlayerUnits() {
-			if p.Name == playerName {
-				target = p
-			}
-		}
-
-		if f.ctx.Data.AreaData.Area == target.Area {
-			step.PrimaryAttack(
-				target.UnitID,
-				4,
-				true,
-				step.Distance(2, 2), // X,Y coords of 2,2 is the perfect hammer angle attack for NPC targeting/attacking, you can adjust accordingly anything between 1,1 - 3,3 is acceptable, where the higher the number, the bigger the distance from the player (usually used for De Seis)
-				step.EnsureAura(skill.Concentration),
-			)
-		} else {
-			var targetAreaNearby = data.Level{}
-			for _, a := range f.ctx.Data.AdjacentLevels {
-				if a.Area == target.Area { // Only pick the first entrance
-					targetAreaNearby = a
-					break // Break immediately after finding first valid entrance
-				}
-			}
-
-			if targetAreaNearby.Position.X != 0 || targetAreaNearby.Position.Y != 0 {
-				_ = action.MoveToArea(targetAreaNearby.Area)
-			} else {
-				_ = action.ClearAreaAroundPlayer(15, f.ctx.Data.MonsterFilterAnyReachable())
-				utils.Sleep(200)
-			}
-		}
-
-		if target.Stats[stat.Life].Value <= 0 {
-			break
+			return action.InteractObjectByID(obj.ID, nil)
 		}
 	}
 
-	return true
+	return errors.New("Waiting for corrected portal")
 }
