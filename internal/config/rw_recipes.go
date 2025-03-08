d7c266ede3bcd9993ec62a0fbf1307a350c97ae9
CarlPoppa <hilikus99@protonmail.com>
2025-03-08 21:55:19 +0000

    Improved runeword recipes struct, add reroll stats Added every runeword recipe, improved base selection

--- internal/config/rw_recipes.go
+++ internal/config/rw_recipes.go
@@ -1,19 +1,21 @@
 package config
 
+import "github.com/hectorgimenez/d2go/pkg/data/item"
+
 var AvailableRunewordRecipes = []string{
 	// List out all the recipe names from runeword_recipes.go
-	"Chains of Honor",
-	"Fortitude",
-	"Call to Arms",
-	"Heart of the Oak",
-	"Flickering Flame",
-	"Smoke",
-	"Rhyme",
-	"Spirit",
-	"Insight",
-	"Cure",
-	"Lore",
-	"Ancients Pledge",
-	"Leaf",
-	"Stealth",
+	string(item.RunewordChainsOfHonor),
+	string(item.RunewordFortitude),
+	string(item.RunewordCallToArms),
+	string(item.RunewordHeartOfTheOak),
+	string(item.RunewordFlickeringFlame),
+	string(item.RunewordSmoke),
+	string(item.RunewordRhyme),
+	string(item.RunewordSpirit),
+	string(item.RunewordInsight),
+	string(item.RunewordCure),
+	string(item.RunewordLore),
+	string(item.RunewordAncientsPledge),
+	string(item.RunewordLeaf),
+	string(item.RunewordStealth),
 }
