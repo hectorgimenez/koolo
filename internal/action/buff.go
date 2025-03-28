a7c18db596e1d822f3ba9aca04aad7989e5e5e8c
Timmynat0r <dj_hollow@hotmail.com>
2025-03-29 00:04:40 +0100

    added map for skilltabs, added stat.AddSkillTab to calculation, minimal refactoring

--- internal/action/buff.go
+++ internal/action/buff.go
@@ -136,7 +136,7 @@ func castSkills(ctx *context.Status, skills []skill.ID) {
 			var found bool
 			kb, found = ctx.Data.KeyBindings.KeyBindingForSkill(skillID)
 			if !found {
-				ctx.Logger.Info("Key binding not found", slog.String("skill", skillID.Desc().Name))
+				ctx.Logger.Info("Key binding not found", slog.String("skill", skill.SkillNames[skillID]))
 				continue
 			}
 			cachedKeys[skillID] = kb
@@ -229,10 +229,6 @@ func ctaFound(d game.Data) bool {
 	return false
 }
 
-// buildGearCache, getspecificSkillBonuses, calculateSlotBonus, should now account for +allSkills ID 127 +classSkills ID 83 and +specificSkills ID 97
-// I have not found a solution for adding +addskilltab ID 188 because skill.go does not have the according trees mapped
-// I think currently ID 188 is giving me a value and a layer, and this layer corresponds to the skill tree, but I am not sure
-// But even if i hardcode these layers to the skilltrees, it will not work because the skill.go does not have the trees mapped to the skills
 func buildGearCache() {
 	ctx := context.Get()
 	ctx.WeaponBonusCache.IsValid = false
@@ -247,7 +243,7 @@ func buildGearCache() {
 	skills := append(ctx.Char.PreCTABuffSkills(), ctx.Char.BuffSkills()...)
 
 	specificBonusesSlot0 := getSpecificSkillBonuses(ctx, skills)
-	slot0Total := calculateSlotBonus(ctx, 0)
+	slot0Total := calculateSlotBonus(ctx)
 	ctx.WeaponBonusCache.Slot0AllClassBonus = slot0Total
 	utils.Sleep(200)
 
@@ -256,7 +252,7 @@ func buildGearCache() {
 	utils.Sleep(500)
 
 	specificBonusesSlot1 := getSpecificSkillBonuses(ctx, skills)
-	slot1Total := calculateSlotBonus(ctx, 1)
+	slot1Total := calculateSlotBonus(ctx)
 	ctx.WeaponBonusCache.Slot1AllClassBonus = slot1Total
 	utils.Sleep(200)
 
@@ -317,19 +313,40 @@ func buildGearCache() {
 
 func getSpecificSkillBonuses(ctx *context.Status, skillIDs []skill.ID) map[skill.ID]int {
 	bonuses := make(map[skill.ID]int)
+
 	for _, skillID := range skillIDs {
+		bonus := 0
+
 		if s, found := ctx.Data.PlayerUnit.Stats.FindStat(stat.SingleSkill, int(skillID)); found {
-			bonuses[skillID] = s.Value
-		} else if s, found := ctx.Data.PlayerUnit.Stats.FindStat(stat.NonClassSkill, int(skillID)); found {
-			bonuses[skillID] = s.Value
+			bonus += s.Value
 
 		}
 
+		if s, found := ctx.Data.PlayerUnit.Stats.FindStat(stat.NonClassSkill, int(skillID)); found {
+			bonus += s.Value
+
+		}
+
+		if tabID, isMapped := SkillToTabs[skillID]; isMapped {
+			if s, found := ctx.Data.PlayerUnit.Stats.FindStat(stat.AddSkillTab, tabID); found {
+				bonus += s.Value
+			}
+		}
+
+		if bonus > 0 {
+			bonuses[skillID] = bonus
+			ctx.Logger.Debug("Skill bonus total",
+				slog.Int("slot", ctx.Data.ActiveWeaponSlot),
+				slog.String("skillname", skill.SkillNames[skillID]),
+				slog.Int("skill_id", int(skillID)),
+				slog.Int("total", bonus))
+		}
 	}
+
 	return bonuses
 }
 
-func calculateSlotBonus(ctx *context.Status, slot int) int {
+func calculateSlotBonus(ctx *context.Status) int {
 	total := 0
 
 	allSkills := 0
@@ -349,12 +366,218 @@ func calculateSlotBonus(ctx *context.Status, slot int) int {
 	}
 	total += classSkills
 
-	ctx.Logger.Debug("Slot Bonus Calculation",
-		slog.Int("slot", slot),
-		slog.Int("all_skills", allSkills),
-		slog.Int("class_skills", classSkills),
-		slog.Int("total", total),
-	)
-
 	return total
+}
+
+// For this to work, right now, we need this List, because Druid Skills and Assassin Skills are missing in "github.com/hectorgimenez/d2go/pkg/data/skill/skilldesc.go"
+var SkillToTabs = map[skill.ID]int{
+	skill.AmplifyDamage:     16,
+	skill.ArcticBlast:       42,
+	skill.Armageddon:        42,
+	skill.Attract:           16,
+	skill.Avoid:             1,
+	skill.AxeMastery:        33,
+	skill.Bash:              32,
+	skill.BattleCommand:     34,
+	skill.BattleCry:         34,
+	skill.BattleOrders:      34,
+	skill.Berserk:           32,
+	skill.BladeFury:         48,
+	skill.BladeSentinel:     48,
+	skill.BladeShield:       48,
+	skill.BladesOfIce:       50,
+	skill.Blaze:             8,
+	skill.BlessedAim:        25,
+	skill.BlessedHammer:     32,
+	skill.Blizzard:          10,
+	skill.BloodGolem:        18,
+	skill.BoneArmor:         17,
+	skill.BonePrison:        17,
+	skill.BoneSpear:         17,
+	skill.BoneSpirit:        17,
+	skill.BoneWall:          17,
+	skill.BurstOfSpeed:      49,
+	skill.CarrionVine:       40,
+	skill.ChainLightning:    9,
+	skill.Charge:            32,
+	skill.ChargedBolt:       9,
+	skill.ChargedBoltSentry: 48,
+	skill.ChargedStrike:     2,
+	skill.ChillingArmor:     10,
+	skill.ClawMastery:       49,
+	skill.ClawsOfThunder:    50,
+	skill.ClayGolem:         18,
+	skill.Cleansing:         26,
+	skill.CloakOfShadows:    49,
+	skill.CobraStrike:       50,
+	skill.ColdArrow:         0,
+	skill.ColdMastery:       10,
+	skill.Concentrate:       32,
+	skill.Concentration:     25,
+	skill.Confuse:           16,
+	skill.Conversion:        32,
+	skill.Conviction:        25,
+	skill.CorpseExplosion:   17,
+	skill.CriticalStrike:    1,
+	skill.CycloneArmor:      42,
+	skill.DeathSentry:       48,
+	skill.Decoy:             1,
+	skill.Decrepify:         16,
+	skill.Defiance:          26,
+	skill.DimVision:         16,
+	skill.Dodge:             1,
+	skill.DoubleSwing:       32,
+	skill.DoubleThrow:       32,
+	skill.DragonClaw:        50,
+	skill.DragonFlight:      50,
+	skill.DragonTail:        50,
+	skill.DragonTalon:       50,
+	skill.Enchant:           8,
+	skill.EnergyShield:      9,
+	skill.Evade:             1,
+	skill.ExplodingArrow:    0,
+	skill.Fade:              49,
+	skill.Fanaticism:        25,
+	skill.Fend:              2,
+	skill.FeralRage:         41,
+	skill.FindItem:          34,
+	skill.FindPotion:        34,
+	skill.FireArrow:         0,
+	skill.FireBlast:         48,
+	skill.FireBolt:          8,
+	skill.FireClaws:         41,
+	skill.FireGolem:         18,
+	skill.FireMastery:       8,
+	skill.FireWall:          8,
+	skill.FireBall:          8,
+	skill.Firestorm:         42,
+	skill.Fissure:           42,
+	skill.FistOfTheHeavens:  32,
+	skill.FistsOfFire:       50,
+	skill.FreezingArrow:     0,
+	skill.Frenzy:            32,
+	skill.FrostNova:         10,
+	skill.FrozenArmor:       10,
+	skill.FrozenOrb:         10,
+	skill.Fury:              41,
+	skill.GlacialSpike:      10,
+	skill.GolemMastery:      18,
+	skill.GrimWard:          34,
+	skill.GuidedArrow:       0,
+	skill.HeartOfWolverine:  40,
+	skill.HolyBolt:          32,
+	skill.HolyFire:          25,
+	skill.HolyFreeze:        25,
+	skill.HolyShield:        32,
+	skill.HolyShock:         25,
+	skill.Howl:              34,
+	skill.Hunger:            41,
+	skill.Hurricane:         42,
+	skill.Hydra:             8,
+	skill.IceArrow:          0,
+	skill.IceBlast:          10,
+	skill.IceBolt:           10,
+	skill.ImmolationArrow:   0,
+	skill.Impale:            2,
+	skill.IncreasedSpeed:    33,
+	skill.IncreasedStamina:  33,
+	skill.Inferno:           8,
+	skill.InnerSight:        1,
+	skill.IronGolem:         18,
+	skill.IronMaiden:        16,
+	skill.IronSkin:          33,
+	skill.Jab:               2,
+	skill.Leap:              32,
+	skill.LeapAttack:        32,
+	skill.LifeTap:           16,
+	skill.Lightning:         9,
+	skill.LightningBolt:     2,
+	skill.LightningFury:     2,
+	skill.LightningMastery:  9,
+	skill.LightningSentry:   48,
+	skill.LightningStrike:   2,
+	skill.LowerResist:       16,
+	skill.Lycanthropy:       41,
+	skill.MaceMastery:       33,
+	skill.MagicArrow:        0,
+	skill.Maul:              41,
+	skill.Meditation:        26,
+	skill.Meteor:            8,
+	skill.Might:             25,
+	skill.MindBlast:         49,
+	skill.MoltenBoulder:     42,
+	skill.MultipleShot:      0,
+	skill.NaturalResistance: 33,
+	skill.Nova:              9,
+	skill.OakSage:           40,
+	skill.Penetrate:         1,
+	skill.PhoenixStrike:     50,
+	skill.Pierce:            1,
+	skill.PlagueJavelin:     2,
+	skill.PoisonCreeper:     40,
+	skill.PoisonDagger:      17,
+	skill.PoisonExplosion:   17,
+	skill.PoisonJavelin:     2,
+	skill.PoisonNova:        17,
+	skill.PolearmMastery:    33,
+	skill.PowerStrike:       2,
+	skill.Prayer:            26,
+	skill.PsychicHammer:     49,
+	skill.Rabies:            41,
+	skill.RaiseSkeleton:     18,
+	skill.Raven:             40,
+	skill.Redemption:        26,
+	skill.ResistCold:        26,
+	skill.ResistFire:        26,
+	skill.ResistLightning:   26,
+	skill.Revive:            18,
+	skill.Sacrifice:         32,
+	skill.Salvation:         26,
+	skill.Sanctuary:         25,
+	skill.ShadowMaster:      49,
+	skill.ShadowWarrior:     49,
+	skill.ShiverArmor:       10,
+	skill.ShockWave:         41,
+	skill.ShockWeb:          48,
+	skill.Shout:             34,
+	skill.RaiseSkeletalMage: 18,
+	skill.SkeletonMastery:   18,
+	skill.SlowMissiles:      1,
+	skill.Smite:             32,
+	skill.SolarCreeper:      40,
+	skill.SpearMastery:      33,
+	skill.SpiritOfBarbs:     40,
+	skill.StaticField:       9,
+	skill.Strafe:            0,
+	skill.Stun:              32,
+	skill.SummonDireWolf:    40,
+	skill.SummonGrizzly:     40,
+	skill.SummonResist:      18,
+	skill.SummonSpiritWolf:  40,
+	skill.Taunt:             34,
+	skill.Teeth:             17,
+	skill.Telekinesis:       9,
+	skill.Teleport:          9,
+	skill.Terror:            16,
+	skill.Thorns:            25,
+	skill.ThrowingMastery:   33,
+	skill.ThunderStorm:      9,
+	skill.TigerStrike:       50,
+	skill.Tornado:           42,
+	skill.Twister:           42,
+	skill.Valkyrie:          1,
+	skill.Vengeance:         32,
+	skill.Venom:             49,
+	skill.Vigor:             26,
+	skill.Volcano:           42,
+	skill.WakeOfFire:        48,
+	skill.WakeOfInferno:     48,
+	skill.WarCry:            34,
+	skill.Warmth:            8,
+	skill.Weaken:            16,
+	skill.WeaponBlock:       49,
+	skill.Werebear:          41,
+	skill.Werewolf:          41,
+	skill.Whirlwind:         32,
+	skill.Zeal:              32,
 }
